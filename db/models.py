f75bf65acd9737b9f9d00c39b961c5e82431b582
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-09 16:48:07 +0700

    update models

--- db/models.py
+++ db/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 class Hospital(models.Model):
     name = models.CharField(max_length=255)
-    adress = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=10)
 
 
 class Department(models.Model):
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+
 
 class Role(models.Model):
     name = models.CharField(max_length=255)
@@ -21,19 +22,19 @@ class Staff(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
 
-class Room(models.Model):
-    department = models.OneToOneField(Department, on_delete=models.CASCADE)
-    room_number = models.IntegerField()
-
-
 class Patient(models.Model):
     name = models.CharField(max_length=255)
-    adress = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=10)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     age = models.IntegerField()
-    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+
+
+class Room(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    room_number = models.IntegerField()
+    patient = models.OneToOneField(Patient, null=True, on_delete=models.SET_NULL)
 
 
 class Accounting(models.Model):
@@ -56,14 +57,14 @@ class Medicine(models.Model):
     amount = models.IntegerField()
 
 
-class Precription(models.Model):
+class Prescription(models.Model):
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    
-    
+
+
 class PrecriptionMedicine(models.Model):
     medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
-    precription = models.ForeignKey(Precription, on_delete=models.CASCADE)
+    precription = models.ForeignKey(Prescription, on_delete=models.CASCADE)
     amount = models.IntegerField()
 
 
