b9fabc05290d571484d7a9ea954273b99820dc1d
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-11-19 22:31:56 +0700

    update diagram

--- console.sql
+++ console.sql
@@ -1,10 +1,10 @@
--- 1
+-- 1 room
 SELECT p.*
 FROM db_patient as p
 JOIN db_room as r ON p.id = r.patient_id
 WHERE r.room_number = 101;
 
--- 2
+-- 2 staff
 SELECT p.*, s.name
 FROM db_staff as s
          JOIN db_patient as p ON s.id = p.staff_id
@@ -13,6 +13,12 @@ where exists (
     from db_role as r
     where s.role_id = r.id
 ) and s.name like '%sak%';
+
+
+-- 2 name
+SELECT p.*
+from db_patient as p
+where p.name like '%%';
 
 
 -- 3. search patient by each department
@@ -27,3 +33,8 @@ FROM db_Patient as p
 JOIN db_disease_patient as pd on p.id = pd.patient_id
 JOIN db_Disease as d on pd.disease_ID = d.id
 WHERE d.name like '%dia%';
+
+SELECT SUM(a.balance) AS Total
+FROM db_accounting as a
+WHERE a.date >= '2023-05-23 00:01:24.863937'and
+    a.date <= '2023-11-19 06:31:24.860249';
